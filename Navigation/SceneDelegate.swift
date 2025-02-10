5bb160e55cf89529b0b8799933642bea6a716ede
nikita123spb <nikita5328858@gmail.com>
2025-02-10 22:08:29 +0300

    Revert "add home work 1"

--- Navigation/SceneDelegate.swift
+++ Navigation/SceneDelegate.swift
@@ -11,30 +11,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-   func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let windowScene = scene as? UIWindowScene else { return }
-    
-        window = UIWindow(windowScene: windowScene)
-
-        let tabBarController = UITabBarController()
-
-        let feedNavigationController = UINavigationController()
-        let profileNavigationController = UINavigationController()
-
-        feedNavigationController.viewControllers = [FeedViewController()]
-        profileNavigationController.viewControllers = [ProfileViewController()]
-       
-        feedNavigationController.tabBarItem = UITabBarItem(title: "Лента", image: UIImage(systemName: "list.bullet"), tag: 0)
-        profileNavigationController.tabBarItem = UITabBarItem(title: "Профиль", image: UIImage(systemName: "person"), tag: 1)
-
-        tabBarController.viewControllers = [feedNavigationController, profileNavigationController]
-
-        window?.rootViewController = tabBarController
-        window?.makeKeyAndVisible()
-}
-}
-    
-    
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
@@ -72,4 +48,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
 
 
+}
 
