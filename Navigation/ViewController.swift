c7cc3bcc51d01a0cf35f5c3b23b8f868fd72fe71
Nikita1 <nikita5328858@gmail.com>
2025-02-10 21:02:16 +0300

    Initial Commit

---
+++ Navigation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Navigation
+//
+//  Created by Nikita  on 10.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
